e759edbf4d2282344040637d2d5fba8135552227
mole828 <mrx800@163.com>
2025-01-09 11:23:03 +0800

    feat: countButton

--- src/jsMain/kotlin/com/example/project/App.kt
+++ src/jsMain/kotlin/com/example/project/App.kt
@@ -4,32 +4,43 @@ import io.kvision.Application
 import io.kvision.CoreModule
 import io.kvision.BootstrapModule
 import io.kvision.BootstrapCssModule
-import io.kvision.core.style
+import io.kvision.core.*
 import io.kvision.html.button
 import io.kvision.html.div
 import io.kvision.module
 import io.kvision.panel.root
 import io.kvision.startApplication
+import io.kvision.theme.Theme
+import io.kvision.theme.ThemeManager
 import io.kvision.utils.em
 import io.kvision.utils.px
 
-
+fun Container.countButton() {
+    var count = 0
+    fun text(count: Int) : String {
+        return "Click: $count"
+    }
+    val btn = button(text(count))
+    btn.onClick {
+        count++
+        btn.text = text(count)
+        console.log(btn.text)
+    }
+}
 
 class App : Application() {
     override fun start() {
         root("kvapp") {
-            val text = div("Hello world")
-            fun a(text: String) = """
-                ${text}!
-            """.trimIndent()
-            text.style {
-                fontSize = 70.px
-                margin = 2.em
-                setStyle("", "")
+            div {
+                style {
+                    height = 10.em
+                    width = 10.em
+                    background = Background(color = Color.name(Col.RED))
+                }
             }
-            button("add !").onClick {
-                text.content?.let { text.content = a(it) }
-            }
+            countButton()
+            countButton()
+            countButton()
         }
     }
 }
